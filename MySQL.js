d6cb98edb362c5d28bd5886cba31f5ca042d386a
Ggoings <42050294+Ggoings@users.noreply.github.com>
2019-01-29 21:57:07 -0600

    Add files via upload

---
+++ MySQL.js
@@ -0,0 +1,10 @@
+Content Discussed :
+
+How to Connect MySQL DB from Node.js
+Execute MySQL Query From Node.js
+Use of MySQL Store Procedure in Node.js.
+
+Before Running this Project
+
+Install npm packages using 'npm install' command.
+Configure MySQL DB Configuration as per yours.
